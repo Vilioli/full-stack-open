2f560432ba0830110dde38541ff6b19514225b7d
Veeti Virtanen <veeti.v.virtanen@gmail.com>
2026-05-30 23:38:39 +0300

    add exercise 1.1 of course-info before its altered state in exercise 1.2

---
+++ part-1-intro-to-react/courseinfo/src/Header.jsx
@@ -0,0 +1,11 @@
+const Header = (props) => {
+    return(
+        <header>
+            <h1>
+                {props.course}
+            </h1>
+        </header>
+    )
+}
+
+export default Header
